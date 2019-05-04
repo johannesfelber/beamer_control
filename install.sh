4ec2060e2b50b2346807509676eb2ae124fe0d0a
johannesfelber <johannesfelber@users.noreply.github.com>
2019-05-04 16:26:21 +0200

    Update install.sh
    Installing dependencies

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 # Install script for beamer_control
 # Author: Johannes Felber www.johannes-felber.at
 
+# Install dependencies
+sudo apt update
+sudo apt install ola-python
+
 # Copy files to /opt/beamer_control
 mkdir /opt/beamer_control
 cp beamer.py config.py /opt/beamer_control/
